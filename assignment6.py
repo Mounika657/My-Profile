a94c26df6e4a6299797e026e406782aa1c4ec883
Mounika657 <59650823+Mounika657@users.noreply.github.com>
2020-05-06 15:59:38 +0530

    Create assignment6.py

---
+++ assignment6.py
@@ -0,0 +1,41 @@
+iBHubs
+
+Home
+AssignmentID - DB006
+Submission Guidelines
+Coding Guidelines
+# Submission Guidelines
+
+Create a folder /home/ec2-user/environment/dbms/dbms_submissions/dbms_assignment_006. Make use of the below example command
+$ mkdir -p /home/ec2-user/environment/dbms/dbms_submissions/dbms_assignment_006
+
+#Coding Guidelines
+Write your queries query.py
+
+Query for each question is to be assigned to a variable in the above python file. Variables for each question are specific individually.
+
+Get fname and lname all the actors casted in movie_id = 12148
+
+Q1="Write your query here"
+...
+
+Count the number of movies in which actor “Harrison (I) Ford” acted
+first name: “Harrison (I)”
+last name: “Ford”
+...
+Q2="Write your query here"
+...
+
+
+Get all the distinct actors ids who acted in all movie whose title starts with Young Latin Girls.
+...
+Q3="Write your query here"
+...
+
+
+How many distinct actors have acted in any movie between 1990 and 2000 (both inclusive).
+...
+Q4="Write your query here"
+...
+
+

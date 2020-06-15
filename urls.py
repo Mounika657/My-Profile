756f0eff7aedb98c1c39f77839ac7872f1ea9358
Mounika657 <59650823+Mounika657@users.noreply.github.com>
2020-06-15 14:41:27 +0530

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,11 @@
+https://images.unsplash.com/photo-1525898939552-346875c89d92?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=giu-vicente-Q_SKLnop3Nc-unsplash.jpg
+https://images.unsplash.com/photo-1588080870173-91a55f9a2f29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=dopic-v_V1CYlhVZY-unsplash.jpg
+
+https://images.unsplash.com/flagged/photo-1572801375968-3ffa529e9173?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=sharon-adams-XV2E6-rRKwI-unsplash.jpg
+
+https://images.unsplash.com/photo-1557768960-c2d136235503?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=alvaro-parra-zokn65LQxp4-unsplash.jpg
+
+https://images.unsplash.com/photo-1432405972618-c60b0225b8f9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=jeffrey-workman-YvkH8R1zoQM-unsplash.jpg
+https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=berkay-gumustekin-ngqyo2AYYnE-unsplash.jpg
+https://images.unsplash.com/photo-1580202313707-46a966af5c6f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=annie-spratt-rti3HfYJzOM-unsplash.jpg
+https://images.unsplash.com/photo-1551826720-4eb3c94651d4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=paul-carroll-pLD6UKygDCU-unsplash.jpg
